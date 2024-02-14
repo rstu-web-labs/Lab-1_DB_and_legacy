0f1e559339b085b6fe144ba81e95cb321d682104
Kzefnde <144488791+Kzefnde@users.noreply.github.com>
2024-02-14 20:33:49 +0300

    Update лаба1.sql

--- src/лаба1.sql
+++ src/лаба1.sql
@@ -2,127 +2,154 @@
 -- Создание схемы raw_data
 CREATE SCHEMA raw_data;
 
--- Создание таблицы sales для загрузки сырых данных
+-- Таблица для сырых данных о продажах автомобилей
 CREATE TABLE raw_data.sales (
-    id SERIAL PRIMARY KEY,
-    auto VARCHAR(255) NOT NULL,
-    gasoline_consumption DECIMAL(5,2),
-    price DECIMAL(10,2) NOT NULL,
-    date DATE NOT NULL,
-    person_name VARCHAR(255) NOT NULL,
-    phone VARCHAR(50) NOT NULL, 
-    discount DECIMAL(5,2),
-    brand_origin VARCHAR(100)
+    id SERIAL PRIMARY KEY, -- Уникальный идентификатор продажи
+    auto VARCHAR(100), -- Марка, модель и цвет автомобиля
+    gasoline_consumption NUMERIC, -- Потребление бензина, л/100км (для электромобилей - NULL)
+    price NUMERIC, -- Цена автомобиля с учетом скидки
+    date DATE, -- Дата покупки
+    person_name VARCHAR(100), -- ФИО покупателя
+    phone VARCHAR(50), -- Телефон покупателя
+    discount SMALLINT, -- Размер скидки в процентах
+    brand_origin VARCHAR(50) -- Страна происхождения бренда
 );
 
--- Загрузка сырых данных из CSV-файла
-COPY raw_data.sales (id, auto, gasoline_consumption, price, date, person_name, phone, discount, brand_origin) FROM 'C:\Users\avb97\Desktop\cars.csv' DELIMITER ',' CSV HEADER NULL 'null';
-
--- Создание схемы car_shop
-CREATE SCHEMA car_shop;
-
--- Создание таблицы cars
-CREATE TABLE car_shop.cars (
-    car_id SERIAL PRIMARY KEY,
-    brand VARCHAR(50) NOT NULL,
-    model VARCHAR(255) NOT NULL,
-    color VARCHAR(50) NOT NULL
+-- Таблица для хранения информации о брендах автомобилей
+CREATE TABLE car_shop.brands (
+    id SERIAL PRIMARY KEY, -- Уникальный идентификатор бренда
+    name VARCHAR(50) NOT NULL -- Название бренда
 );
 
--- Создание таблицы purchases
+-- Таблица для хранения информации о моделях автомобилей, включая цвет
+CREATE TABLE car_shop.car_models (
+    id SERIAL PRIMARY KEY, -- Уникальный идентификатор модели
+    brand_id INTEGER REFERENCES car_shop.brands(id), -- Ссылка на уникальный идентификатор бренда
+    name VARCHAR(100) NOT NULL, -- Название модели
+    color VARCHAR(50) -- Цвет автомобиля
+);
+
+-- Таблица для хранения информации о покупателях автомобилей
+CREATE TABLE car_shop.customers (
+    id SERIAL PRIMARY KEY, -- Уникальный идентификатор покупателя
+    name VARCHAR(100) NOT NULL, -- ФИО покупателя
+    phone VARCHAR(50) UNIQUE -- Телефон покупателя (уникальный)
+);
+
+-- Таблица для хранения информации о покупках автомобилей, включая связи с покупателями и сырыми данными о продажах
 CREATE TABLE car_shop.purchases (
-    purchase_id SERIAL PRIMARY KEY,
-    car_id INT REFERENCES car_shop.cars(car_id),
-    sale_id INT REFERENCES raw_data.sales(id),
-    CONSTRAINT unique_purchase UNIQUE(car_id, sale_id)
+    id SERIAL PRIMARY KEY, -- Уникальный идентификатор покупки
+    sales_id INTEGER UNIQUE REFERENCES raw_data.sales(id), -- Ссылка на уникальный идентификатор продажи (сырые данные)
+    customer_id INTEGER REFERENCES car_shop.customers(id), -- Ссылка на уникальный идентификатор покупателя
+    discount SMALLINT, -- Размер скидки в процентах
+    date DATE -- Дата покупки
 );
+-- Немного о самих таблицах
+--  raw_data.sales: Таблица для сырых данных о продажах автомобилей.
+--  car_shop.brands: Таблица для хранения информации о брендах автомобилей.
+--  car_shop.car_models: Таблица для хранения информации о моделях автомобилей, включая цвет.
+--  car_shop.customers: Таблица для хранения информации о покупателях автомобилей.
+--  car_shop.purchases: Таблица для хранения информации о покупках автомобилей, включая связи с покупателями и сырыми данными о продажах
 
--- Заполнение таблицы cars данными
-INSERT INTO car_shop.cars (brand, model, color)
-SELECT DISTINCT
-    SUBSTRING(auto FROM 1 FOR POSITION(' ' IN auto) - 1) AS brand,
-    SUBSTRING(auto FROM POSITION(' ' IN auto) + 1 FOR POSITION(',' IN auto) - POSITION(' ' IN auto) - 1) AS model,
-    SUBSTRING(auto FROM POSITION(',' IN auto) + 2) AS color
+-- Загрузка данных из CSV-файла в таблицу raw_data.sales
+COPY raw_data.sales FROM 'C:\Users\avb97\Desktop\cars.csv' DELIMITER ',' CSV HEADER NULL 'null';
+
+-- Загрузка данных в таблицу brands
+INSERT INTO car_shop.brands (name)
+SELECT DISTINCT substring(auto from 1 for position(' ' in auto) - 1)
 FROM raw_data.sales;
 
--- Заполнение таблицы purchases данными
-INSERT INTO car_shop.purchases (car_id, sale_id)
-SELECT
-    c.car_id,
-    s.id
-FROM
-    raw_data.sales s
-JOIN
-    car_shop.cars c ON SUBSTRING(s.auto FROM 1 FOR POSITION(' ' IN s.auto) - 1) = c.brand
-    AND SUBSTRING(s.auto FROM POSITION(' ' IN s.auto) + 1 FOR POSITION(',' IN s.auto) - POSITION(' ' IN s.auto) - 1) = c.model
-    AND SUBSTRING(s.auto FROM POSITION(',' IN s.auto) + 2) = c.color;
+-- Загрузка данных в таблицу car_models
+INSERT INTO car_shop.car_models (brand_id, name, color)
+SELECT b.id, substring(auto from position(' ' in auto) + 1), substring(auto from position(',' in auto) + 2)
+FROM raw_data.sales r
+JOIN car_shop.brands b ON substring(r.auto from 1 for position(' ' in r.auto) - 1) = b.name;
 
--- Создание индекса для ускорения JOIN операций
-CREATE INDEX idx_car_id ON car_shop.purchases(car_id);
-CREATE INDEX idx_sale_id ON car_shop.purchases(sale_id);
+-- Загрузка данных в таблицу customers
+INSERT INTO car_shop.customers (name, phone)
+SELECT DISTINCT person_name, phone FROM raw_data.sales;
 
+-- Загрузка данных в таблицу purchases
+INSERT INTO car_shop.purchases (sales_id, customer_id, discount, date)
+SELECT s.id, c.id, discount, date
+FROM raw_data.sales s
+JOIN car_shop.customers c ON s.person_name = c.name AND s.phone = c.phone;
+
+
+--Аналитические скрипты:
 
 -- Задание 1
+-- Запрос для вычисления процента моделей автомобилей, у которых отсутствует параметр gasoline_consumption.
 SELECT 
-    (COUNT(*) FILTER (WHERE gasoline_consumption IS NULL) * 100.0 / COUNT(*)) AS nulls_percentage_gasoline_consumption
+    ROUND(COUNT(CASE WHEN gasoline_consumption IS NULL THEN 1 END) * 100.0 / COUNT(*), 2) AS nulls_percentage_gasoline_consumption
 FROM 
     raw_data.sales;
 
 -- Задание 2
-SELECT 
-    SUBSTRING(s.auto FROM 1 FOR POSITION(' ' IN s.auto) - 1) AS brand_name,
+-- Запрос для вычисления средней цены автомобилей бренда по всем годам с учетом скидки.
+-- Результат сгруппирован по бренду и году, отсортирован по бренду и году.
+SELECT
+    b.name AS brand_name,
     EXTRACT(YEAR FROM s.date) AS year,
     ROUND(AVG(s.price * (1 - s.discount / 100.0)), 2) AS price_avg
-FROM 
+FROM
     raw_data.sales s
-GROUP BY 
-    brand_name, year
-ORDER BY 
-    brand_name, year;
+JOIN
+    car_shop.brands b ON substring(s.auto from 1 for position(' ' in s.auto) - 1) = b.name
+GROUP BY
+    b.name, EXTRACT(YEAR FROM s.date)
+ORDER BY
+    b.name, year;
 
--- Задание 3
-SELECT 
+--Задание 3
+-- Запрос для вычисления средней цены всех автомобилей с разбивкой по месяцам в 2022 году с учетом скидки.
+-- Результат сгруппирован по месяцам и году, отсортирован по месяцам.
+SELECT
     EXTRACT(MONTH FROM s.date) AS month,
     EXTRACT(YEAR FROM s.date) AS year,
     ROUND(AVG(s.price * (1 - s.discount / 100.0)), 2) AS price_avg
-FROM 
+FROM
     raw_data.sales s
-WHERE 
+WHERE
     EXTRACT(YEAR FROM s.date) = 2022
-GROUP BY 
-    month, year
-ORDER BY 
-    month;
+GROUP BY
+    EXTRACT(MONTH FROM s.date), EXTRACT(YEAR FROM s.date)
+ORDER BY
+    EXTRACT(MONTH FROM s.date);
 
 -- Задание 4
-SELECT 
-    s.person_name AS person,
-    STRING_AGG(c.brand || ' ' || c.model, ', ') AS cars
-FROM 
+-- Запрос для получения списка купленных машин у каждого пользователя через запятую.
+-- Результат сгруппирован по имени пользователя и отсортирован по имени пользователя.
+SELECT
+    c.name AS person,
+    STRING_AGG(substring(s.auto from position(' ' in s.auto) + 1), ', ' ORDER BY s.date) AS cars
+FROM
     raw_data.sales s
-JOIN 
-    car_shop.purchases p ON s.id = p.sale_id
-JOIN 
-    car_shop.cars c ON p.car_id = c.car_id
-GROUP BY 
-    person
-ORDER BY 
-    person;
+JOIN
+    car_shop.customers c ON s.person_name = c.name AND s.phone = c.phone
+GROUP BY
+    c.name;
 
 -- Задание 5
-SELECT 
+-- Запрос для нахождения самой большой и самой маленькой цены продажи автомобиля с разбивкой по стране без учета скидки.
+-- Результат сгруппирован по стране.
+SELECT
     brand_origin,
     MAX(price) AS price_max,
     MIN(price) AS price_min
-FROM 
+FROM
     raw_data.sales
-GROUP BY 
+GROUP BY
     brand_origin;
 
+
 -- Задание 6
-SELECT 
+-- Запрос для вычисления количества всех пользователей из США (пользователей с номерами телефонов, начинающихся на +1).
+SELECT
     COUNT(*) AS persons_from_usa_count
-FROM 
-    raw_data.sales
-WHERE 
+FROM
+    car_shop.customers
+WHERE
     phone LIKE '+1%';
+
+
