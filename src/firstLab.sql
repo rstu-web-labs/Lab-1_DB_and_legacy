37d9ba00add8c19f08f2800ee18b1c415682bbb6
Ryksovv <remics246@gmail.com>
2024-02-16 13:21:32 +0300

    Update first

--- src/firstLab.sql
+++ src/firstLab.sql
@@ -2,8 +2,8 @@
 create table sales(
 id integer primary key,
 auto varchar(30),
-gasoline_consumption varchar(30),
-price float,
+gasoline_consumption numeric(4, 1),
+price numeric(10, 2),
 date date,
 person_name varchar(30),
 phone varchar(30),
@@ -28,7 +28,7 @@ create table color(
 	name varchar(30) unique not null -- название цвета, состоит из символов, уникальное и не может быть пустым полем
 );
 
-create table client(
+create table buyer(
 	id serial primary key, -- идентификатор, автоинкремент, первичный ключ
 	name varchar(30) not null, -- имя и фамилия клиента (соответственно, строковый тип), поле не может быть пустым
 	phone varchar(30) -- номер телефона покупателя; т.к. нет определенного шаблона в сырых данных, то берём строковый тип 
@@ -38,7 +38,7 @@ create table brand (
 	id serial primary key, -- идентификатор, автоинкремент, первичный ключ
 	name varchar(30) unique not null, -- название бренда => используем строковый тип; названия не должны повторяться, т.к. от бренда следуют различные модели авто (поэтому unique); не может быть пустым полем
 	country_origin_id integer, -- идентификатор, внешний ключ, используем integer, т.к. берём целочисленный тип от первичного ключа из таблицы "Country"
-	foreign key (country_origin_id) references Country(id)
+	foreign key (country_origin_id) references Country(id) on delete restrict
 );
 
 create table model (
@@ -46,20 +46,20 @@ create table model (
 	brand_id integer, -- идентификатор, внешний ключ, используем integer, т.к. берём целочисленный тип от первичного ключа из таблицы "Brand"
 	name varchar(30) not null, -- название модели автомобиля -> пишем символами, поэтому строковый varchar; поле не может быть пустым
 	color_id integer, -- идентификатор, внешний ключ, используем integer, т.к. берём целочисленный тип от первичного ключа из таблицы "Color"
-	gas_consumption float check (gas_consumption > 0), -- просто число с плавающей точкой (дано изначально в исходном файле); P.S. отел юзать тип DECIMAL с параметрами (2, 2), но не срослось, не хотело работать
-	foreign key (brand_id) references Brand(id),
-	foreign key (color_id) references Color(id)
+	gas_consumption numeric(4, 1) check (gas_consumption > 0), -- просто число с плавающей точкой (дано изначально в исходном файле); P.S. отел юзать тип DECIMAL с параметрами (2, 2), но не срослось, не хотело работать
+	foreign key (brand_id) references Brand(id) on delete restrict,
+	foreign key (color_id) references Color(id) on delete restrict
 );
 
 create table sales(
 	id serial primary key, -- идентификатор, автоинкремент, первичный ключ
 	model_id integer, -- идентификатор, внешний ключ, используем integer, т.к. берём целочисленный тип от первичного ключа из таблицы "Model"
 	buyer_id integer, -- идентификатор, внешний ключ, используем integer, т.к. берём целочисленный тип от первичного ключа из таблицы "Buyer"
-	price float check (price >= 0), -- цена - используем число с плавающей точкой; P.S. хотел использовать тип MONEY, но он жёстко и жестоко ругался при импорте данных
+	price numeric(10, 2) check (price >= 0), -- цена - используем число с плавающей точкой; P.S. хотел использовать тип MONEY, но он жёстко и жестоко ругался при импорте данных
 	discount integer check (discount between 0 and 100), -- скидка, в исходном файле не содержит дробных чисел, следовательно, достаточно использовать целочисленный тип
 	sell_date date, -- поле с датой, соответственно, содержит тип даты
-	foreign key (model_id) references Model(id),
-	foreign key (buyer_id) references Buyer(id)
+	foreign key (model_id) references Model(id) on delete restrict,
+	foreign key (buyer_id) references Buyer(id) on delete restrict
 );
 
 -- Заполнение таблиц 
@@ -73,7 +73,7 @@ where brand_origin is not null;
 
 -- Цвет
 
-insert into color (name)
+insert into car_shop.color (name)
 select distinct split_part(auto, ', ', 2)
 from raw_data.sales;
 
@@ -87,7 +87,7 @@ from raw_data.sales;
 
 insert into brand (name, country_origin_id)
 select distinct split_part(split_part(auto, ', ', 1), ' ', 1) as brand_name,
-				coalesce (country.id, (select id from country where name = 'Unknown')) as country_id
+				coalesce (country.id, (select id from country where name is null)) as country_id
 from raw_data.sales
 left join country on raw_data.sales.brand_origin = country.name;
 
@@ -128,7 +128,7 @@ group by model.id, buyer.id, raw_data.sales.price, raw_data.sales.discount, raw_
 select 
 	count(*) * 100.0 / (select count(*) from model) as nulls_percentage_gasoline_consumption
 from model
-where gas_consumption is null
+where gas_consumption is null;
 
 -- Запрос 2. Запрос, который покажет название бренда и среднюю цену его автомобилей в разбивке по всем годам с учётом скидки.
 
