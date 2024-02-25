5bce61ddf2c7fadc0c6d552cee56d5afb8610f3e
Andrey Demianov <gappointgap@gmail.com>
2024-02-25 23:01:00 +0200

    lab01 update

--- src/Lab1.sql
+++ src/Lab1.sql
@@ -1,139 +1,223 @@
 create database life_on_wheels;
 
+
 create schema raw_data;
 
-CREATE TABLE raw_data.sales (
-    id serial,
-    auto VARCHAR(255),
-    gasoline_consumption DECIMAL(10, 2),
-    price DECIMAL(15, 2),
-    date DATE,
-    person_name VARCHAR(255),
-    phone VARCHAR(50), 
-    discount INT,
-    brand_origin VARCHAR(50)
-);
+create table raw_data.sales (
+	id INT ,
+	auto VARCHAR(50),
+	gasoline_consumption NUMERIC,
+	price NUMERIC,
+	date DATE,
+	person_name VARCHAR(50),
+	phone VARCHAR(50),
+	discount numeric,
+	brand_origin VARCHAR(50)
+)
+
+COPY raw_data.sales(id, auto, gasoline_consumption, price, date, person_name, phone, discount, brand_origin) FROM '/home/user/Загрузки/cars.csv' DELIMITER ',' CSV HEADER NULL 'null';
 
 
-COPY sales(id, auto, gasoline_consumption, price, date, person_name, phone, discount, brand_origin) FROM '/home/user/Загрузки/cars.csv' DELIMITER ',' CSV HEADER NULL 'null';
-
+select * from raw_data.sales s ;
 
 create schema car_shop;
 
+create table car_shop.country (
+	id serial primary key,
+	brand_origin varchar(50) not null unique
+)
+
+
+create table car_shop.brand (
+	id serial primary key,
+	id_country int,
+	brand varchar(50) not null unique,
+	foreign key (id_country) references car_shop.country(id) on delete cascade
+)
+
+create table car_shop.model (
+	id serial primary key,
+	model varchar(50) not null unique
+)
+
+create table car_shop.color (
+	id serial primary key,
+	color varchar(50) not null unique
+)
 
 create table car_shop.cars (
-	cars_id serial, 
-	brand varchar(50) not null,
-	model varchar(50) not null, 
-	color varchar(50) not null, 
-	constraint cars_id_key primary key(cars_id)
+	id serial primary key,
+	id_brand int references car_shop.brand(id) on delete cascade,
+	id_model int references car_shop.model(id) on delete cascade, 
+	id_color int references car_shop.color(id) on delete cascade,
+	gasoline_consumption numeric check (gasoline_consumption > 0 and gasoline_consumption<100 )
+)
+
+create table car_shop.clients(
+	id serial primary key,
+	name varchar(30) not null,
+	surname varchar(30) not null,
+	phone varchar(50) not null unique
+)
+
+create table car_shop.sales (
+	id serial primary key,
+	id_car int references car_shop.cars(id) on delete cascade,
+	id_buyer int references car_shop.clients(id) on delete cascade,
+	price numeric not null,
+	discount numeric,
+	date date not null
 )
 
 
-create table car_shop.auto_details (
-    auto_id serial, 
-    cars_id int, 
-    gasoline_consumption DECIMAL(10, 2),
-    price DECIMAL(15, 2),
-    date DATE,
-    constraint auto_id_key primary key(auto_id), 
-    constraint cars_id_foreign_key foreign key (cars_id) references car_shop.cars(cars_id)
-);
-
-create table car_shop.owners (
-	owner_id serial, 
-	auto_id int, 
-	preson_name varchar(50), 
-	phone varchar(50), 
-	discount int, 
-	brand_origin VARCHAR(50),
-	constraint owner_id_key primary key(owner_id),
-	constraint auto_id_foreign_key foreign key (auto_id) references car_shop.auto_details(auto_id)
-)
+-- заполнение данными новых таблиц
+insert into car_shop.country  (brand_origin)
+select brand_origin as brand_origin 
+from raw_data.sales 
+where brand_origin is not null
+group by brand_origin ;
 
 
-insert into car_shop.cars (brand, model, color)
+select c.brand_origin  from car_shop.country c ;
+
+
+insert into car_shop.brand(id_country, brand)
+select distinct
+case
+    when s.brand_origin is null then null
+    else c.id
+  end as id_country,
+  substring(s.auto, 1, position(' ' IN s.auto) - 1) as brand
+from raw_data.sales s
+left join car_shop.country c on c.brand_origin = s.brand_origin or s.brand_origin is null
+group by c.id, brand, s.brand_origin, s.auto;
+
+select * from car_shop.brand b ;
+
+
+insert into car_shop.model(model)
+select SUBSTRING(auto, POSITION(' ' in auto) + 1, POSITION(',' in auto) - POSITION(' ' in auto) - 1) as model
+from raw_data.sales 
+group by model;
+
+select * from car_shop.model m ;
+
+insert into car_shop.color(color)
+select SUBSTRING(auto, POSITION(',' in auto) + 2) as color
+from raw_data.sales 
+group by color;
+
+select * from car_shop.color c ;
+
+
+insert into car_shop.cars (id_brand, id_model, id_color, gasoline_consumption)
+select b.id,  m.id, c.id, s.gasoline_consumption 
+from raw_data.sales s
+left join car_shop.brand b 
+on b.brand = substring(s.auto, 1, position(' ' in s.auto) - 1) 
+left join car_shop.model m 
+on m.model  = substring (s.auto, position(' ' in s.auto) + 1, position(',' in s.auto) - position(' ' in s.auto) - 1)
+left join car_shop.color c 
+on c.color = substring(s.auto, position(',' in s.auto) + 2)
+group by b.id,  m.id, c.id, s.gasoline_consumption ;
+
+select * from car_shop.cars c  ;
+
+insert into car_shop.clients (name, surname, phone)
 select 
-    SUBSTRING(auto, 1, POSITION(' ' in auto) - 1) as brand,
-    SUBSTRING(auto, POSITION(' ' in auto) + 1, POSITION(',' in auto) - POSITION(' ' in auto) - 1) as model,
-    SUBSTRING(auto, POSITION(',' in auto) + 2) as color
-from raw_data.sales;
+substring(s.person_name, 1, position (' ' in s.person_name) - 1) as name,
+substring(s.person_name, position (' ' in s.person_name) + 1) as surname,
+s.phone  as phone
+from raw_data.sales s
+group by 
+substring(s.person_name, 1, position (' ' in s.person_name) - 1),
+substring(s.person_name, position (' ' in s.person_name) + 1),
+s.phone ;
 
-insert into car_shop.auto_details 
-select 
-	ROW_NUMBER() over (order by 1) as cars_id,
-	gasoline_consumption as gasoline_consumption,
-	price as price,
-	date as date
-from raw_data.sales;	
+select *  from car_shop.clients cl ;
 
 
-insert into car_shop.owners  
-select 
-	ROW_NUMBER() over (order by 1) as auto_id ,
-	person_name as preson_name,
-	phone as phone,
-	discount as discount,
-	brand_origin as brand_origin
-from raw_data.sales;	
+insert into car_shop.sales(id_car, id_buyer, price, discount, "date")
+select c.id , c3.id, s.price, s.discount , s."date"
+from raw_data.sales s 
+inner join car_shop.cars c on
+	substring(s.auto, 1, position(' ' in s.auto) - 1) = (select brand from car_shop.brand where id = c.id_brand  ) and
+	substring (s.auto, position(' ' in s.auto) + 1, position(',' in s.auto) - position(' ' in s.auto) - 1) = (select model from car_shop.model where id = c.id_model) and
+	substring(s.auto, position(',' in s.auto) + 2) = (select color from car_shop.color where id = c.id_color) 
+inner join car_shop.clients c3 on
+	c3.phone = s.phone and c3."name"  = substring(s.person_name, 1, position (' ' in s.person_name) - 1) and
+	c3.surname = substring(s.person_name, position (' ' in s.person_name) + 1)
+	
+select * from car_shop.sales;
+
 
 --Напишите запрос, который выведет процент моделей машин, у которых нет параметра gasoline_consumption.
-SELECT COUNT(*) * 100.0 / (SELECT COUNT(*) FROM car_shop.auto_details) AS nulls_percentage_gasoline_consumption
-FROM car_shop.auto_details
-WHERE gasoline_consumption IS NULL;
+select COUNT(*) * 100.0 / (select COUNT(*) from car_shop.cars) as nulls_percentage_gasoline_consumption
+from car_shop.cars
+where gasoline_consumption is null;
+
+
 --Напишите запрос, который покажет название бренда и среднюю цену его автомобилей в разбивке по всем годам с учётом скидки. 
 --Итоговый результат отсортируйте по названию бренда и году в восходящем порядке. 
 --Среднюю цену округлите до второго знака после запятой. Формат итоговой таблицы:
-select c.brand as brand_name,
-extract(year from ad.date) as year, 
-ROUND(AVG(ad.price), 2) AS price_avg
-from car_shop.cars c
-join car_shop.auto_details ad on c.cars_id  = ad.cars_id 
-group by c.brand, extract(year from ad.date)
-order by c.brand, extract(year from ad.date)
+
+select b.brand as brand_name,
+extract (year from s.date) as year,
+round(avg(s.price), 2) as price_avg
+from car_shop.sales s 
+join car_shop.cars c on c.id = s.id_car 
+join car_shop.brand b on b.id = c.id_brand 
+group by brand_name, year
+order by brand_name, year;
+
+
 
 --Посчитайте среднюю цену всех автомобилей с разбивкой по месяцам в 2022 году с учётом скидки. 
 --Результат отсортируйте по месяцам в восходящем порядке. Среднюю цену округлите до второго знака после запятой.
 select 
-extract (month from  ad.date ) as month,
-extract (year from  ad.date ) as year,
-ROUND(AVG(ad.price), 2) AS price_avg
-from  car_shop.auto_details ad 
-where extract (year from  ad.date ) = 2022
-group by extract (month from  ad.date ), extract (year from  ad.date )
-order by extract (month from  ad.date )
+	extract(month from s.date) as month,
+	extract(year from s.date) as year,
+	round(avg(s.price), 2) as price_avg
+from car_shop.sales s 
+where extract(year from s.date)=2022
+group by month, year
+order by extract(month from s.date);
+
 
 ---Используя функцию STRING_AGG, напишите запрос, который выведет список купленных машин у каждого пользователя через запятую. 
 ---Пользователь может купить две одинаковые машины — это нормально. Название машины покажите полное, с названием бренда — например: Tesla Model 3. 
 ---Отсортируйте по имени пользователя в восходящем порядке. Сортировка внутри самой строки с машинами не нужна.
 
-select  o.preson_name as person, 
-string_agg(c.brand || ' ' || c.model, ', ' order by c.brand, c.model) as cars
-from car_shop.owners o
-join car_shop.auto_details ad  on o.auto_id = ad.auto_id 
-join car_shop.cars c on ad.cars_id  = c.cars_id 
-group by o.preson_name
-order by o.preson_name
+select c."name" , c.surname ,
+string_agg(b.brand || ' ' || m.model, ', ' ) as cars
+from car_shop.sales s 
+join car_shop.clients c  on c.id = s.id_buyer 
+join car_shop.cars c2 on c2.id  = s.id_car 
+join car_shop.brand b on b.id  = c2.id_brand 
+join car_shop.model m on m.id  = c2.id_model 
+group by c."name",c.surname 
+order by c."name",c.surname 
+
 
 --Напишите запрос, который вернёт самую большую и самую маленькую цену продажи автомобиля с разбивкой по стране без учёта скидки. 
 --Цена в колонке price дана с учётом скидки.
 
-select 
-o.brand_origin as brand_origin, 
-max(ad.price) as price_max,
-min(ad.price) as price_min
-from
-car_shop.auto_details ad 
-join car_shop.owners o on ad.auto_id  = o.auto_id 
-where o.brand_origin is not null
-group by o.brand_origin
-order by o.brand_origin
+select c2.brand_origin,
+max(s.price) as price_max,
+min (s.price) as price_min
+from car_shop.sales s 
+join car_shop.cars c on c.id = s.id_car 
+join car_shop.brand b on b.id = c.id_brand 
+left join car_shop.country c2 on c2.id = b.id_country
+where c2.brand_origin is not null
+group by  c2.brand_origin
+order by c2.brand_origin;
+
 
 --Напишите запрос, который покажет количество всех пользователей из США. 
 --Это пользователи, у которых номер телефона начинается на +1.
 select count(*) as persons_from_usa_count 
-from car_shop.owners o 
+from car_shop.clients 
 where phone like '+1%';
-
 
 
