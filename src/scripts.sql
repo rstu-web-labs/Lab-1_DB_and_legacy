f359a6725e51c208a910e40ba93c321941fabd84
Yaro Bor <borovinscky.yaroslaw@yandex.ru>
2024-02-26 14:56:11 +0300

    Borovinsky Laba1 done

--- src/scripts.sql
+++ src/scripts.sql
@@ -35,18 +35,29 @@ CREATE TABLE car_shop.country ( -- таблица стран
 CREATE TABLE car_shop.models_list ( -- таблица моделей
   model_id SERIAL PRIMARY KEY, -- первичный ключ
   model VARCHAR(15), -- символьный тип для названия модели
-  gasoline_consumption float4 -- вещественный тип для расхода топлива
 );
 
+CREATE TABLE car_shop.brands ( -- таблица брендов
+  brand_id SERIAL PRIMARY KEY, -- первичный ключ
+  brand_name VARCHAR(20),  -- символьный тип для названия бренда
+  brand_country_id INTEGER, -- целочисленный тип для внешнего ключа страны бренда
+  constraint fk_brand_country_id foreign key (brand_country_id) REFERENCES country (country_id)
+);
 
 CREATE TABLE car_shop.cars ( -- таблица машин
   car_id SERIAL PRIMARY KEY, --  первичный ключ
-  brand_name VARCHAR(30), -- символьный тип для имени бренда
   model_list_id INTEGER, -- целочисленный тип для внешнего ключа моделей
-  brand_original_id INTEGER, -- целочисленный тип для внешнего ключа стран
-  color_id INTEGER, -- целочисленный тип для внешнего ключа цветов
+  brand_id INTEGER, -- целочисленный тип для внешнего ключа бренда
   CONSTRAINT fk_model_id FOREIGN KEY (model_list_id) REFERENCES models_list (model_id),
-  CONSTRAINT fk_brand_original FOREIGN KEY (brand_original_id) REFERENCES country (country_id),
+  CONSTRAINT fk_brand_id FOREIGN KEY (brand_id) REFERENCES brands (brand_id)
+);
+
+CREATE TABLE car_shop.cars_eque ( -- таблица комплектаций машин
+  car_eque_id SERIAL PRIMARY KEY,  --  первичный ключ
+  car_id INTEGER,  -- целочисленный тип для внешнего ключа машины
+  color_id INTEGER, -- целочисленный тип для внешнего ключа цветов
+  gasoline_consumption float4, -- вещественный тип для расхода топлива
+  CONSTRAINT fk_car_id FOREIGN KEY (car_id) REFERENCES cars (car_id),
   CONSTRAINT fk_color FOREIGN KEY (color_id) REFERENCES colors (id)
 );
 
@@ -75,20 +86,44 @@ SELECT DISTINCT split_part(substring(auto from '[^,]+$'), ' ', 2) AS color
 FROM raw_data.sales;
 
 INSERT INTO car_shop.models_list (model, gasoline_consumption) -- заполняем таблицу моделей машин
-SELECT DISTINCT substring(auto from '\w+\s(.*?),') AS model, gasoline_consumption
+SELECT DISTINCT substring(auto from '\w+\s(.*?),') AS model
 FROM raw_data.sales;
 
-INSERT INTO car_shop.cars (brand_name, model_list_id, brand_original_id, color_id) -- крестимся и заполняем таблицу машин
+INSERT INTO car_shop.brands (brand_name, brand_country_id)  -- заполняем таблицу брендов
 SELECT
-    distinct split_part(auto, ' ', 1) AS brand_name,
-    (SELECT model_id FROM models_list WHERE model = substring(auto from '\w+\s(.*?),')),
-    (SELECT country_id FROM country WHERE country_name = s.brand_origin),
-    (SELECT id FROM colors WHERE color = split_part(substring(auto from '[^,]+$'), ' ', 2))
+	DISTINCT split_part(auto, ' ', 1) AS brand_name,
+	(SELECT country_id FROM country WHERE country_name = s.brand_origin)
+FROM raw_data.sales s;
+
+INSERT INTO car_shop.cars (brand_id, model_list_id)  -- заполняем таблицу машин
+select DISTINCT
+    (SELECT brand_id FROM brands WHERE brand_name = split_part(auto, ' ', 1)),
+    (SELECT model_id FROM models_list WHERE model = substring(auto from '\w+\s(.*?),'))
 FROM
     raw_data.sales s;
 
-INSERT INTO car_shop.sales (price, discount, date, person_id) -- заполняем таблицу продаж
+INSERT INTO car_shop.cars_eque (car_id, color_id, gasoline_consumption) -- заполняем таблицу комплектаций машин
+select DISTINCT
+    (SELECT car_id FROM cars ca
+    	inner join car_shop.brands br ON ca.brand_id = br.brand_id
+    	inner join car_shop.models_list ml ON ca.model_list_id = ml.model_id
+    	WHERE br.brand_name = split_part(s.auto, ' ', 1)
+    	AND ml.model = substring(auto from '\w+\s(.*?),')),
+    (SELECT id FROM colors WHERE color = split_part(substring(auto from '[^,]+$'), ' ', 2)),
+    gasoline_consumption
+FROM
+    raw_data.sales s;
+
+INSERT INTO car_shop.sales (sold_car_id, price, discount, date, person_id) -- заполняем таблицу продаж
 select
+	(select car_eque_id from cars_eque eq
+		inner join car_shop.colors cl ON eq.color_id = cl.id
+		inner join car_shop.cars ca ON eq.car_id = ca.car_id
+		inner join car_shop.brands br ON ca.brand_id = br.brand_id
+    	inner join car_shop.models_list ml ON ca.model_list_id = ml.model_id
+    	WHERE br.brand_name = split_part(s.auto, ' ', 1)
+    	and ml.model = substring(auto from '\w+\s(.*?),')
+    	and cl.color = split_part(substring(auto from '[^,]+$'), ' ', 2)),
 	s.price,
 	s.discount,
 	s.date,
@@ -97,30 +132,23 @@ select
 FROM
     raw_data.sales s;
 
-UPDATE car_shop.sales SET sold_car_id = ( -- всё ещё заполняем таблицу продаж, но теперь только id проданной машины
-    SELECT c.car_id
-    FROM car_shop.cars c
-    JOIN car_shop.models_list ml ON c.model_list_id = ml.model_id
-    JOIN car_shop.colors cl ON c.color_id = cl.id
-    WHERE c.brand_name = split_part(auto, ' ', 1)
-    AND ml.model = substring(auto from '\w+\s(.*?),')
-    AND cl.color = split_part(substring(auto from '[^,]+$'), ' ', 2)
-)
-FROM raw_data.sales
-WHERE car_shop.sales.sales_id = raw_data.sales.id;
 -- заполнение выполнено, можно начинать аналитические скрипты (─‿‿─)
 -- сприпт 1:
 SELECT
     (COUNT(model_id) FILTER (WHERE gasoline_consumption IS NULL) * 100 / COUNT(model_id)) AS percent
 FROM
-    models_list;
+    car_shop.cars_eque eq
+    inner join car_shop.cars ca ON eq.car_id = ca.car_id
+    inner join car_shop.models_list ml ON ca.model_list_id = ml.model_id;
 
 -- сприпт 2:
 SELECT
     brand_name,
     EXTRACT(year FROM date) AS year,
     ROUND(AVG(price)::numeric, 2) AS price_avg
-from car_shop.sales, car_shop.cars
+from car_shop.sales, car_shop.cars_eque eq
+	inner join car_shop.cars ca ON eq.car_id = ca.car_id
+	inner join car_shop.brands br ON ca.brand_id = br.brand_id
 GROUP by brand_name, year
 ORDER by brand_name, year ASC;
 
@@ -135,27 +163,27 @@ GROUP by month, year
 order by month ASC;
 
 -- сприпт 4:
-CREATE OR REPLACE VIEW car_shop.person_auto_view AS -- создаём вьюшку и радуемся
-SELECT s.sold_car_id, c.brand_name, m.model, p.person_name
-FROM car_shop.sales s
-JOIN cars c ON s.sold_car_id = c.car_id
+SELECT p.person_name AS person,
+STRING_AGG(b.brand_name || ' ' || m.model, ', ') AS cars
+FROM sales s
+JOIN cars_eque ce ON s.sold_car_id = ce.car_eque_id
+JOIN cars c ON ce.car_id = c.car_id
 JOIN models_list m ON c.model_list_id = m.model_id
-join persons p on s.person_id = p.person_id;
+JOIN brands b ON c.brand_id = b.brand_id
+JOIN persons p ON s.person_id = p.person_id
+GROUP BY p.person_name
+ORDER BY p.person_name;
 
-select person_name as person,
-STRING_AGG(CONCAT(brand_name, ' ', model), ', ') AS cars
-FROM person_auto_view pa
-GROUP BY person
-ORDER BY person ASC;
-
--- сприпт 5:  Тут я не захотел делать вьюшку
+-- сприпт 5:
 SELECT
 	co.country_name as brand_origin,
 	MAX(s.price + (s.price * s.discount / 100)) AS price_max,
 	MIN(s.price + (s.price * s.discount / 100)) AS price_min
-FROM car_shop.sales s
-JOIN cars c ON s.sold_car_id = c.car_id
-JOIN country co ON c.brand_original_id = co.country_id
+FROM sales s
+JOIN cars_eque eq ON s.sold_car_id = eq.car_eque_id
+JOIN cars c ON eq.car_id = c.car_id
+JOIN brands b ON c.brand_id = b.brand_id
+JOIN country co ON b.brand_country_id = co.country_id
 GROUP BY co.country_name
 ORDER BY co.country_name ASC;
 
